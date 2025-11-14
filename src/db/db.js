84222b5e58bf4c6d6d0ba44b468c0aa4afc956e7
Rohan Kumar <rohit22001166@gmail.com>
2025-11-14 22:33:32 +0530

    add database connection constant for MongoDB

--- src/db/db.js
+++ src/db/db.js
@@ -1,12 +1,14 @@
 import mongoose from "mongoose";
+import { DB_NAME } from "../constant.js";
 
 const connectDB = async ()=>{
     try{
-        const connectionInstance= await mongoose.connect(`${process.env.MONGO_URI}/wingametube`);
+        const connectionInstance= await mongoose.connect(`${process.env.MONGO_URI}/${DB_NAME}`);
         console.log("Mongo connection done",connectionInstance);
 
     }catch(error){
-        console.log("connection failed while connected with Mongo",error.message)
+        console.log("connection failed while connected with Mongo",error.message);
+        process.exit(1);
     }
 }
 
